6fa88b2dd6b16dc43b1713891881911786d8f4f2
Daniel Beck <090302@5409.dk>
2023-06-22 16:46:17 +0200

    Feat: post Bible verses referenced in messages

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import discord from 'discord.js'
 import 'dotenv/config'
 import config from './config'
+import bible from './bible'
 
 const client = new discord.Client({
-  intents: ['Guilds']
+  intents: ['Guilds', 'GuildMessages', 'MessageContent']
 })
 
 client.on('guildCreate', async guild => {
@@ -12,6 +13,22 @@ client.on('guildCreate', async guild => {
   if (typeof member === 'undefined') return
 
   member.setNickname(config.nickname)
+})
+
+client.on('messageCreate', async message => {
+  if (message.author.bot) return
+
+  const references = bible.extractReferences(message.content)
+
+  if (references.length === 0) return
+
+  const promises = references.map(reference => {
+    return message.channel.send(
+      reference.quote({ form: 'embed', inline: false })
+    )
+  })
+
+  await Promise.all(promises)
 })
 
 client.login(process.env['DISCORD_TOKEN'])
