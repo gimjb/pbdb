ba78acbecc8938924ab3943625e81d0d2f43b73e
Daniel Beck <090302@5409.dk>
2023-06-28 14:49:18 +0200

    Refactor: `toDiscordMarkdown(text: string)`
    Move conversion to Discord markdown to a separate function.

--- src/commands/changelog.ts
+++ src/commands/changelog.ts
@@ -2,21 +2,12 @@ import fs from 'fs'
 import path from 'path'
 import type ApplicationCommand from './ApplicationCommand'
 import config from '../config'
+import toDiscordMarkdown from '../utils/toDiscordMarkdown'
 
 function processChangelog(changelog: string) {
   return (
     '> ' +
-    changelog
-      // Remove links that are not supported in Discord Markdown.
-      .replace(/\[.+?]: http.+\n/g, '')
-      .replace(/\[(?<linkText>.+?)](?!\()/g, '$<linkText>')
-      // Horizontal rules
-      .replace(/^-{3,}/g, '—')
-      // Unwrap wrapped lines.
-      .replace(/(?<!\n| {2,})\n(?!(?:\n|[-*\d]| {2,}))/gm, ' ')
-      .trim()
-      // Blockquote the whole changelog.
-      .replace(/\n/g, '\n> ') +
+    toDiscordMarkdown(changelog).trim().replace(/\n/g, '\n> ') +
     // Add a link to the changelog in the Git repository.
     '\n\nThis changelog can also be found in the official [GitHub repository](' +
     `${config.officialRepository}/blob/master/docs/CHANGELOG.md).`
---
+++ src/utils/toDiscordMarkdown.ts
@@ -0,0 +1,12 @@
+export default function toDiscordMarkdown(text: string) {
+  return (
+    text
+      // Remove links that are not supported in Discord Markdown.
+      .replace(/\[.+?]: http.+\n/g, '')
+      .replace(/\[(?<linkText>.+?)](?!\()/g, '$<linkText>')
+      // Horizontal rules
+      .replace(/^-{3,}/g, '—')
+      // Unwrap wrapped lines.
+      .replace(/(?<!\n| {2,})\n(?!(?:\n|[-*\d]| {2,}))/gm, ' ')
+  )
+}
